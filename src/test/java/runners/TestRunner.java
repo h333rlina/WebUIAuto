439032af7f9bab80353535219896fc45c2ab47ed
Herlina <herlinaayu356@gmail.com>
2025-05-15 13:58:47 +0700

    Fix: update feature path in TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/feature",
         glue = {"stepdef"},
         plugin = {"pretty", "html:target/cucumber-report.html"},
         monochrome = true
